dc5f902edf0d224e1dffbbf011fc05318ee5a154
RGFRv2 <38694732+RGFRv2@users.noreply.github.com>
2026-09-20 21:24:46 +0200

    Keep bookmark bundles merge focused

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -32,6 +33,8 @@ void main() async {
 
   HttpOverrides.global = MyHttpOverrides();
 
+  await dotenv.load(fileName: '.env');
+
   final sharedPreferences = await SharedPreferences.getInstance();
 
   final appInfo = await PackageInfo.fromPlatform();
@@ -48,12 +51,11 @@ void main() async {
         ),
       );
 
-  const sentryDsn = String.fromEnvironment('SENTRY_DSN');
-  const enableSentry = bool.fromEnvironment('ENABLE_SENTRY');
-  if ((kReleaseMode || enableSentry) && sentryDsn.isNotEmpty) {
+  if ((kReleaseMode && (dotenv.env['SENTRY_DSN'] != null && dotenv.env['SENTRY_DSN'] != "")) ||
+      (dotenv.env['ENABLE_SENTRY'] == "true" && (dotenv.env['SENTRY_DSN'] != null && dotenv.env['SENTRY_DSN'] != ""))) {
     SentryFlutter.init(
       (options) {
-        options.dsn = sentryDsn;
+        options.dsn = dotenv.env['SENTRY_DSN'];
         options.sendDefaultPii = false;
         options.beforeSend = sentryHandleError;
       },
@@ -96,10 +98,7 @@ class _MyAppState extends ConsumerState<MyApp> {
           ReceiveSharingIntent.instance.reset();
         }
       },
-      onError: (e, stackTrace) {
-        Sentry.captureException(e, stackTrace: stackTrace);
-        return null;
-      },
+      onError: (e, stackTrace) => Sentry.captureException(e, stackTrace: stackTrace),
     );
   }
 
